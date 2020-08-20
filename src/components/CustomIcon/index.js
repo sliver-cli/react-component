4891f0e85d61e4681ad5fd7afcff0d80ed60d241
tangkaiqiang <justforgit@163.com>
2020-08-20 15:16:54 +0800

    添加自定义图标组件

---
+++ src/components/CustomIcon/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './index.scss';
+import logo from '../../assets/logo.png';
+
+const CustomIcon = () => <img src={logo} alt="icon" />;
+
+export default CustomIcon;
--- src/route/index.js
+++ src/route/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import BasicHeader from '../components/BasicHeader';
+import CustomIcon from '../components/CustomIcon';
 
 const AppRoute = () => (
   <Switch>
     <Route path="/BasicHeader" component={BasicHeader} />
+    <Route path="/CustomIcon" component={CustomIcon} />
   </Switch>
 );
 
